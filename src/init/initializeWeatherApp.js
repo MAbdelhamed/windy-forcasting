9a754c02547fe569e61bd5d6ea6a03588b176505
Mohamed Abdelhamed <56353109+MAbdelhamed@users.noreply.github.com>
2021-12-06 18:22:15 +0100

    style main divs

--- src/init/initializeWeatherApp.js
+++ src/init/initializeWeatherApp.js
@@ -7,13 +7,13 @@ import {
 } from '../utils/DOMUtils.js';
 import { cityBox } from '../views/cityWeather.js';
 
-async function initialize() {
+export async function initialize() {
   const globalWeatherContainer = getDOMElement('global-forecasting');
 
-  // globalWeatherContainer.appendChild(await cityBox('New York'));
-  // globalWeatherContainer.appendChild(await cityBox('Cairo'));
-  // globalWeatherContainer.appendChild(await cityBox('Amsterdam'));
-  // const requestedWeatherContainer = getDOMElement('requested-forecasting');
+  globalWeatherContainer.appendChild(await cityBox('New York'));
+  globalWeatherContainer.appendChild(await cityBox('Cairo'));
+  globalWeatherContainer.appendChild(await cityBox('Amsterdam'));
+  const requestedWeatherContainer = getDOMElement('requested-forecasting');
 
   requestedWeatherContainer.appendChild(await cityBox('Cairo'));
 }
--- src/views/cityWeather.js
+++ src/views/cityWeather.js
@@ -6,6 +6,7 @@ import {
 } from '../utils/DOMUtils.js';
 import { fetchWeather } from '../API/fetchAPI.js';
 import { setWeatherStatusImage } from '../handlers/weatherHandler.js';
+import { initialize } from '../init/initializeWeatherApp.js';
 
 function getDate(now) {
   const months = [
@@ -37,7 +38,7 @@ function getDate(now) {
 }
 // function that build the view for each city container with its elements
 
-export async function cityBox(city = '') {
+export async function cityBox(city) {
   const cityContainer = createDOMElement('div');
   clearDOMElement(cityContainer);
   const cityName = createDOMElement('h2');
@@ -45,7 +46,7 @@ export async function cityBox(city = '') {
   let now = new Date();
   date.textContent = getDate(now);
   const temperature = createDOMElement('h3');
-  const weatherStatus = createDOMElement('div');
+  const weatherStatus = createDOMElement('h3');
   const weatherStatusImage = createDOMElement('img');
   const highLow = createDOMElement('h4');
 
@@ -59,9 +60,7 @@ export async function cityBox(city = '') {
       const weather = await fetchWeather(city);
       cityName.textContent = `${weather.name} , ${weather.sys.country}`;
       temperature.textContent = `${Math.round(weather.main.temp)} °C`;
-      weatherStatus.style.display = 'flex';
-      weatherStatus.style.flexDirection = 'row';
-
+      temperature.className = 'temp';
       weatherStatus.textContent = `${weather.weather[0].main}`;
       // weatherStatusImage = await setWeatherStatusImage(
       //   getDate(now),
@@ -71,9 +70,13 @@ export async function cityBox(city = '') {
       highLow.textContent = `${Math.round(
         weather.main.temp_min
       )} °C / ${Math.round(weather.main.temp_max)} °C`;
-      cityContainer.id = weather.name;
+      cityContainer.className = 'cityBox';
     } catch (error) {
-      throw console.log(error);
+      setTimeout(function () {
+        alert('Invalid city name ,Please Enter a valid city name');
+        initialize();
+      }, 1000);
+      throw error(error.message);
     }
     cityContainer.appendChild(cityName);
     cityContainer.appendChild(date);
