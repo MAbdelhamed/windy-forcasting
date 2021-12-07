31f6e95e68e5239e809a12e47ca68d4c8168cb06
Mohamed Abdelhamed <56353109+MAbdelhamed@users.noreply.github.com>
2021-12-07 18:46:02 +0100

    style and edite readme file

--- src/init/initializeWeatherApp.js
+++ src/init/initializeWeatherApp.js
@@ -3,19 +3,26 @@
 
 import { getDOMElement, clearDOMElement } from '../utils/DOMUtils.js';
 import { cityBox } from '../views/cityWeatherView.js';
+import { cityNames } from '../data.js';
 
 export async function initialize() {
   const globalWeatherContainer = getDOMElement('global-forecasting');
   const requestedCity = getDOMElement('search-box');
   clearDOMElement(globalWeatherContainer);
-  // get the weather forecast for 3 specific cities
-  globalWeatherContainer.appendChild(await cityBox('New York'));
-  globalWeatherContainer.appendChild(await cityBox('Cairo'));
-  globalWeatherContainer.appendChild(await cityBox('Amsterdam'));
+  // get the weather forecast for 3 random cities
+  globalWeatherContainer.appendChild(
+    await cityBox(cityNames[Math.floor(Math.random() * cityNames.length)])
+  );
+  globalWeatherContainer.appendChild(
+    await cityBox(cityNames[Math.floor(Math.random() * cityNames.length)])
+  );
+  globalWeatherContainer.appendChild(
+    await cityBox(cityNames[Math.floor(Math.random() * cityNames.length)])
+  );
   const requestedWeatherContainer = getDOMElement('requested-forecasting');
   // get the weather forecast for a user requested city
-  requestedCity.addEventListener('keypress', async (evnt) => {
-    if (evnt.key === 'Enter') {
+  requestedCity.addEventListener('keypress', async (event) => {
+    if (event.key === 'Enter') {
       const cityForecast = await cityBox(requestedCity.value);
       clearDOMElement(requestedWeatherContainer);
       requestedWeatherContainer.appendChild(requestedCity);
